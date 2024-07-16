2adcfd4d3783c75421493682e682830a387f06ed
Neyoong <neyoollimbu@gmail.com>
2024-07-16 15:24:46 +0545

    first

---
+++ REPL.py
@@ -0,0 +1,6 @@
+# REPL stands for read , evaluate , print and loop.
+# comments are used to write something that is just used for explaining the code or marking it is not included in the code . 
+# We can use comments by typing [ ctrl+K+C ]
+# There are two types of comments ;
+# single line comment: just write a hashtag infront of the comment
+# multiple line comment: just write a  """"  infront of the comment 
--- hello.py
+++ hello.py
@@ -1,2 +1,14 @@
 # print is a function in python for printing anything in the screen
 print("Hello World ")
+print(""" Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle Twinkle Little Star
+How I wonder what you are!
+Twinkle Twinkle, Little Star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle Twinkle Little Star
+How I wonder what you are! """)
